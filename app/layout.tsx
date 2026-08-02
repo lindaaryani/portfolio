830956a4adc6104457a0df9b367f0e8e4d353c4c
lindaaryani <122853787+lindaaryani@users.noreply.github.com>
2026-08-02 21:44:35 +0800

    fix dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
           <Analytics />
         </ThemeProvider>
